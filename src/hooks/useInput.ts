8c9ffcefde8766c8d8058572111ee4191a7817f5
Dmitry Verteyko <verteyko1990@gmail.com>
2022-05-03 17:50:17 +0300

    add new hook useInput

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -12,7 +12,8 @@ export const useInput = (initialValue: string): IUseInput => {
     const [value, setValue] = useState<string>(initialValue);
 
     const onChange = (event: ChangeEvent<HTMLInputElement>): void => {
-        setValue(event.target.value);
+        const { target: { value } } = event;
+        setValue(value);
     }
 
     return {
