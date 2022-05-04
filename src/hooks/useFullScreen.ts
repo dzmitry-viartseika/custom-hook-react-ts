dd01c8cd9fad1a8692c59510cdf74ef0b01518a7
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-05-04 10:21:53 +0300

    add new hook useFullscreen

--- src/hooks/useFullScreen.ts
+++ src/hooks/useFullScreen.ts
@@ -3,9 +3,16 @@ import {useRef, useState} from 'react';
 export const useFullScreen = () => {
     const element = useRef<HTMLImageElement>(null);
     const triggerFullScreen = () => {
-        if (element.current) {
-            element.current.requestFullscreen();
+        if (element?.current?.requestFullscreen) {
+            element?.current.requestFullscreen();
         }
+        // else if (element.current.mozRequestFullscreen) {
+        //     element.current.mozRequestFullscreen();
+        // } else if (element.current.webkitRequestFullscreen) {
+        //     element.current.webkitRequestFullscreen();
+        // } else if (element.current.msRequestFullscreen) {
+        //     element.current.msRequestFullscreen();
+        // }
     }
 
     const exitFull = () => {
